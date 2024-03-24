655fcdd63ae0a90ef890cf65840fd11112077189
Jamil Bashir <jamil@themindgauge.com>
2024-03-24 14:34:14 +0500

    Created UI for create issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuePage = () => {
   return (
     <div>
-      IssuePage<Button>Test</Button>
+      IssuePage
+      <Button>
+        <Link href="issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
